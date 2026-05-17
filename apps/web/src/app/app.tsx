a8ea75b2fe03d905cbab3a0e60f6a8bf1456f690
Blank Eugen <blank.eugeny@yandex.ru>
2026-05-17 18:45:17 +0200

    chore: remove light theme

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,13 +1,12 @@
-import { ThemeProvider } from "@/components/theme";
 import { RouterProvider } from "@tanstack/react-router";
 import { Toaster } from "@/components/ui/sonner";
 import { router } from "./routes";
 
 export function App() {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+    <>
       <RouterProvider router={router} />
       <Toaster />
-    </ThemeProvider>
+    </>
   );
 }
