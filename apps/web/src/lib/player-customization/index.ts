f18cf5f55c2052f6009bb3cc984569185bdfe47e
Blank Evgeniy <blank.eugeny@yandex.ru>
2026-05-29 12:58:30 +0200

    chore(client): add player icons

--- apps/web/src/lib/player-customization/index.ts
+++ apps/web/src/lib/player-customization/index.ts
@@ -4,3 +4,10 @@ export {
   playerColorClasses,
   type PlayerColor,
 } from "./player-colors";
+
+export {
+  PLAYER_ICONS,
+  DEFAULT_PLAYER_ICON,
+  playerIconComponents,
+  type PlayerIcon,
+} from "./player-icons";
---
+++ apps/web/src/lib/player-customization/player-icons.ts
@@ -0,0 +1,44 @@
+import type { ComponentType } from "react";
+
+import type { IconProps } from "@/components/icons/types";
+
+import { IconEmojiAngel } from "@/components/icons/icon-emoji-angel";
+import { IconEmojiCrying } from "@/components/icons/icon-emoji-crying";
+import { IconEmojiEvil } from "@/components/icons/icon-emoji-evil";
+import { IconEmojiLaughing } from "@/components/icons/icon-emoji-laughing";
+import { IconEmojiMedicalMask } from "@/components/icons/icon-emoji-medical-mask";
+import { IconEmojiSmileDizzy } from "@/components/icons/icon-emoji-smile-dizzy";
+import { IconEmojiStarFace } from "@/components/icons/icon-emoji-star-face";
+import { IconEmojiTongueWinkRight } from "@/components/icons/icon-emoji-tongue-wink-right";
+import { IconEmojiVomiting } from "@/components/icons/icon-emoji-vomiting";
+
+export const PLAYER_ICONS = [
+  "angel",
+  "crying",
+  "evil",
+  "laughing",
+  "medical-mask",
+  "smile-dizzy",
+  "star-face",
+  "tongue-wink-right",
+  "vomiting",
+] as const;
+
+export type PlayerIcon = (typeof PLAYER_ICONS)[number];
+
+export const playerIconComponents: Record<
+  PlayerIcon,
+  ComponentType<IconProps>
+> = {
+  angel: IconEmojiAngel,
+  crying: IconEmojiCrying,
+  evil: IconEmojiEvil,
+  laughing: IconEmojiLaughing,
+  "medical-mask": IconEmojiMedicalMask,
+  "smile-dizzy": IconEmojiSmileDizzy,
+  "star-face": IconEmojiStarFace,
+  "tongue-wink-right": IconEmojiTongueWinkRight,
+  vomiting: IconEmojiVomiting,
+};
+
+export const DEFAULT_PLAYER_ICON: PlayerIcon = "angel";
